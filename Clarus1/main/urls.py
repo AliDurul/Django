87383e9698d88594fcd8b03e0bc715fc55daf22e
pmammedov <pmammedov@gmail.com>
2024-07-05 12:12:08 +0200

    chore: Add dscohort and fscohort apps with basic files and configurations

--- Clarus1/main/urls.py
+++ Clarus1/main/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+# from fscohort.views import home
+# from dscohort.views import dshome
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('home/', home),
+    # path('dscohort/', dshome),
+    path('fs/', include('fscohort.urls')),
+    path('ds/', include('dscohort.urls')),
 ]
--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # Django-Training
 
+django-admin startproject projectname .
+
+py -m venv env
+
 source ./env/Scripts/Activate
 
+pip install -r requirements.txt
+
+pip freeze > requirements.txt
+
+python manage.py migrate
+
+pyhton manage.py startapp fscohort
